6ff92ab583c08274f41ed5e2abd4c6736fa977f3
cqx <542784252@qq.com>
2024-08-01 16:51:37 +0800

    触底加载

--- src/views/Home/components/HomeMain.vue
+++ src/views/Home/components/HomeMain.vue
@@ -1,87 +1,107 @@
 <template>
-<div class="home-main">
-  <!-- 轮播图 -->
-  <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-    <van-swipe-item
-      v-for="(item, index) in swipeData"
-      :key="index"
-    >
-      <img :src="item.img" alt="">
-    </van-swipe-item>
-  </van-swipe>
-
-  <!-- 菜单 -->
-  <van-grid icon-size="40">
-    <van-grid-item v-for="(item, index) in logoData" :key="index" :icon="item.img" :text="item.info[0].value" />
-  </van-grid>
-
-  <!-- 滚动提示 -->
-  <van-notice-bar left-icon="fire-o" :scrollable="false">
-    <span>热点资讯公告：</span>
-    <van-swipe
-      vertical
-      class="notice-swipe"
-      :autoplay="3000"
-      :show-indicators="false"
-    >
-      <van-swipe-item v-for="(item,index) in newsData" :key="index" v-text="item.chiild[0].val"></van-swipe-item>
+  <div class="home-main">
+    <!-- 轮播图 -->
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="(item, index) in swipeData" :key="index">
+        <img :src="item.img" alt="" />
+      </van-swipe-item>
     </van-swipe>
-  </van-notice-bar>
 
-  <!-- 商品列表 -->
-  <product-list :products-data="productsData"></product-list>
-</div>
+    <!-- 菜单 -->
+    <van-grid icon-size="40">
+      <van-grid-item
+        v-for="(item, index) in logoData"
+        :key="index"
+        :icon="item.img"
+        :text="item.info[0].value"
+      />
+    </van-grid>
+
+    <!-- 滚动提示 -->
+    <van-notice-bar left-icon="fire-o" :scrollable="false">
+      <span>热点资讯公告：</span>
+      <van-swipe
+        vertical
+        class="notice-swipe"
+        :autoplay="3000"
+        :show-indicators="false"
+      >
+        <van-swipe-item
+          v-for="(item, index) in newsData"
+          :key="index"
+          v-text="item.chiild[0].val"
+        ></van-swipe-item>
+      </van-swipe>
+    </van-notice-bar>
+
+    <!-- 商品列表 -->
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="initProductsData"
+    >
+      <product-list :products-data="productsData"></product-list>
+    </van-list>
+  </div>
 </template>
 
 <script setup>
-import { getDefalutData } from '@/api/home.js'
-import { getProductsData } from '@/api/product.js'
-import { computed } from '@vue/reactivity';
-import ProductList from '@/components/ProductList.vue';
-import { ref } from 'vue'
+import { getDefalutData } from "@/api/home.js";
+import { getProductsData } from "@/api/product.js";
+import { computed } from "@vue/reactivity";
+import ProductList from "@/components/ProductList.vue";
+import { ref } from "vue";
 
-let indexData = ref({})
-let productsData = ref([])
+let indexData = ref({});
+let productsData = ref([]);
 
 const initIndexData = async () => {
   const { data } = await getDefalutData();
   if (data.status !== 200) {
-    return
+    return;
   }
-  indexData.value = data.data
-}
+  indexData.value = data.data;
+};
 
-let page = 1
+const loading = ref(false);
+const finished = ref(false);
+let page = 1;
+let limit = 4;
 const initProductsData = async () => {
+  loading.value = true;
   const { data } = await getProductsData({
-    limit: 4,
-    page
-  })
+    limit,
+    page,
+  });
   if (data.status !== 200) {
-    return
+    return;
   }
-  productsData.value.push(...data.data)
-  page++
-}
+  loading.value = false;
+  productsData.value.push(...data.data);
+  if (data.data.length < limit) {
+    finished.value = true;
+  }
+  page++;
+};
 
 const swipeData = computed(() => {
-  return indexData.value.swiperBg?.default.imgList.list
-})
+  return indexData.value.swiperBg?.default.imgList.list;
+});
 
 const logoData = computed(() => {
-  return indexData.value.menus?.default.imgList.list
-})
+  return indexData.value.menus?.default.imgList.list;
+});
 
 const newsData = computed(() => {
-  return indexData.value.news?.default.newList.list
-})
+  return indexData.value.news?.default.newList.list;
+});
 
-initIndexData()
-initProductsData()
+initIndexData();
 </script>
 
 <style scoped lang="scss">
-.my-swipe img{
+.my-swipe img {
   width: 100%;
   height: 170px;
 }
